3ee96db46733a8aaa1dba0a1ae64b859baa816b2
YancoubaDiatta-Capgemini <163310003+YancoubaDiatta-Capgemini@users.noreply.github.com>
2024-03-15 14:52:03 +0100

    Create script2WithProcess.py

---
+++ script2WithProcess.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import requests
+from io import StringIO
+
+# URL du fichier CSV sur GitHub
+url = "https://raw.githubusercontent.com/YancoubaDiatta-Capgemini/testPy2DKU/main/testData.csv"
+
+# Récupérer le contenu du fichier CSV à partir de l'URL
+response = requests.get(url)
+csv_data = StringIO(response.text)
+
+# Charger le fichier CSV dans un DataFrame
+data = pd.read_csv(csv_data)
+
+# Créer une nouvelle colonne en multipliant la colonne "age" par 2
+data['age_doubled'] = data['Age'] * 2
+
+#print(data.head())
