137461d5ee3ec0e9671b7e0f7f4eea5a82ff6024
YancoubaDiatta-Capgemini <163310003+YancoubaDiatta-Capgemini@users.noreply.github.com>
2024-03-15 11:12:50 +0100

    Create scriptPython.py
    injection test

---
+++ scriptPython.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import requests
+from io import StringIO
+import dataiku
+# URL du fichier CSV sur GitHub
+url = "https://raw.githubusercontent.com/YancoubaDiatta-Capgemini/testPy2DKU/main/testData.csv"
+
+# Récupérer le contenu du fichier CSV à partir de l'URL
+response = requests.get(url)
+csv_data = StringIO(response.text)
+
+# Charger le fichier CSV dans un DataFrame
+data = pd.read_csv(csv_data)
+# Créer un dataset à partir du DataFrame
+output_dataset = dataiku.Dataset("output_prep8")
+output_dataset.write_with_schema(data)
