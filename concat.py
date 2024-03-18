ef7c3e55151df7d5defbf90f9294bfee170471ee
MariBQ <75124333+MariBQ@users.noreply.github.com>
2024-03-18 11:09:55 +0100

    Add files via upload

---
+++ concat.py
@@ -0,0 +1,12 @@
+# create a pandas dataframe to concatenate 2 csv files
+import pandas as pd
+
+# Create a pandas dataframe to concatenate 2 csv files
+df1 = pd.read_csv("C:\\Users\\mbeloque\\OneDrive - Capgemini\\Documents\\Python projets\\donnees1.csv")
+df2 = pd.read_csv("C:\\Users\\mbeloque\\OneDrive - Capgemini\\Documents\\Python projets\\donnees2.csv")
+
+# Concatenate the 2 dataframes
+df = pd.concat([df1, df2], axis=1)
+
+# Print the concatenated dataframe
+print("Concatenated DataFrame:\n", df)
