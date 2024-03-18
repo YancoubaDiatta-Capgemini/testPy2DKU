7b030ad8ea962b3ef0e3346ca056ca5920d63ca2
YancoubaDiatta-Capgemini <163310003+YancoubaDiatta-Capgemini@users.noreply.github.com>
2024-03-18 16:20:33 +0100

    Update concat.py

--- concat.py
+++ concat.py
@@ -2,8 +2,8 @@
 import pandas as pd
 
 # Create a pandas dataframe to concatenate 2 csv files
-df1 = pd.read_csv("C:\\Users\\mbeloque\\OneDrive - Capgemini\\Documents\\Python projets\\donnees1.csv")
-df2 = pd.read_csv("C:\\Users\\mbeloque\\OneDrive - Capgemini\\Documents\\Python projets\\donnees2.csv")
+df1 = pd.read_csv("https://raw.githubusercontent.com/YancoubaDiatta-Capgemini/testPy2DKU/main/donnees1.csv")
+df2 = pd.read_csv("https://raw.githubusercontent.com/YancoubaDiatta-Capgemini/testPy2DKU/main/donnees2.csv")
 
 # Concatenate the 2 dataframes
 df = pd.concat([df1, df2], axis=1)
